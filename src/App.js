e4a061ea290bf3e4d8a07623da7b3f663e7b2a45
DaniBencz <daniel.bencz@gmail.com>
2023-02-03 14:54:22 +0100

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>My REACT_APP_VERSION is {process.env.REACT_APP_VERSION}</p>
+        <p>My MY_FOO does not work, as it is called incorrectly {process.env.MY_FOO}</p>
+        <p>My NODE_ENV is {process.env.NODE_ENV}</p>
+        <a
+          className="App-link"
+          href="https://create-react-app.dev/docs/adding-custom-environment-variables/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          About Env Vars in React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
